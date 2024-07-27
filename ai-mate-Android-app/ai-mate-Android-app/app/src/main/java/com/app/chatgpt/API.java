58eb7814d2ce2f5cb905bc8ccb37a485b6b083f1
SACHIN KUMAR <118509936+sachinroyyy@users.noreply.github.com>
2024-07-27 12:36:36 +0530

    Delete ai-mate-Android-app/ai-mate-Android-app directory

--- ai-mate-Android-app/ai-mate-Android-app/app/src/main/java/com/app/chatgpt/API.java
+++
@@ -1,6 +0,0 @@
-package com.app.chatgpt;
-
-public class API {
-    public static String API_URL = "https://api.openai.com/v1/completions";
-    public static String API = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-}
